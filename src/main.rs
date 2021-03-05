3cd3e0c076ed1d35a05e2a78ff43db2d1c64327d
Benjamin Kästner <benjamin.kaestner@gmail.com>
2021-03-05 17:41:31 +0100

    Remove manual buffer handling

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,9 @@
 use anyhow::Result;
-use humanize_rs::bytes::Bytes;
 use indicatif::{ProgressBar, ProgressStyle};
 use std::fs::File;
-use std::io::{Read, Write};
+use std::io::{self, Read};
 use std::path::PathBuf;
 use structopt::StructOpt;
-
-fn parse_bytes(src: &str) -> Result<usize, &'static str> {
-    if let Ok(bytes) = src.parse::<Bytes>() {
-        Ok(bytes.size())
-    } else {
-        Err("Invalid size format")
-    }
-}
 
 #[derive(Debug, StructOpt)]
 #[structopt(name = "pipeviewer", about = "A pipe inspecting application.")]
@@ -20,10 +11,6 @@ struct Opt {
     /// Input file, stdin if not specified
     #[structopt(parse(from_os_str))]
     input: Option<PathBuf>,
-
-    /// Internal buffer size
-    #[structopt(short, long, name = "SIZE", parse(try_from_str = parse_bytes), default_value = "2MiB")]
-    buffer_size: usize,
 }
 
 fn main() -> Result<()> {
@@ -54,13 +41,6 @@ fn main() -> Result<()> {
     };
     let mut output = pb.wrap_write(std::io::stdout());
 
-    let mut buffer = vec![0; opts.buffer_size];
-
-    while let Ok(n) = input.read(&mut buffer) {
-        if n == 0 {
-            break;
-        }
-        output.write_all(&buffer[..n])?;
-    }
+    io::copy(&mut input, &mut output)?;
     Ok(())
 }
