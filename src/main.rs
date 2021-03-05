5e13364882cd20d27e24edd028d74c64f9a26769
Benjamin Kästner <benjamin.kaestner@gmail.com>
2021-03-05 12:25:25 +0100

    Prepare argument parsing with structopt

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,26 @@
+use humanize_rs::bytes::Bytes;
 use std::io::{Read, Write};
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+fn parse_bytes(src: &str) -> Result<usize, &'static str> {
+    if let Ok(bytes) = src.parse::<Bytes>() {
+        Ok(bytes.size())
+    } else {
+        Err("Invalid size format")
+    }
+}
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "pipeviewer", about = "A pipe inspecting application.")]
+struct Opt {
+    /// Input file, stdin if not specified
+    #[structopt(parse(from_os_str))]
+    input: Option<PathBuf>,
+
+    #[structopt(short, long, parse(try_from_str = parse_bytes), default_value = "2MiB")]
+    buffer_size: usize,
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut input = std::io::stdin();
